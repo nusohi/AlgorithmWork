044c3d210aa210923072545d44d6517579bb6a9d
nuso <nelohjb@hotmail.com>
2019-12-11 09:58:46 +0800

    Fix: 循环赛奇数情况，活动安排先排序

--- src/app/Activity.java
+++ src/app/Activity.java
@@ -3,7 +3,7 @@
  * @Author: nuso
  * @LastEditors: nuso
  * @Date: 2019-11-24 13:53:10
- * @LastEditTime: 2019-11-24 14:14:25
+ * @LastEditTime: 2019-12-06 10:08:30
  */
 package app;
 
@@ -11,8 +11,11 @@ class Activity {
     public static void main(String[] args) {
         int[] start = new int[] { 1, 3, 0, 5, 3, 5, 6, 8, 8, 2, 12 };
         int[] finish = new int[] { 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14 }; // len=11 (0-10)
+        // start = new int[] { 5, 2, 9, 7, 4 };
+        // finish = new int[] { 6, 9, 15, 10, 5 };
         boolean[] X = new boolean[start.length];
 
+        Sort(start, finish);
         Arrange(start, finish, X);
         Show(start, finish, X);
     }
@@ -30,6 +33,15 @@ class Activity {
     }
 
     private static void Show(int[] start, int[] finish, boolean[] X) {
+        StringBuilder array = new StringBuilder();
+        for (int i : start) {
+            array.append(i + "\t");
+        }
+        array.append("\n");
+        for (int i : finish) {
+            array.append(i + "\t");
+        }
+
         int last = 0;
         StringBuilder s = new StringBuilder();
         for (int i = 0, len = finish.length; i < len; i++) {
@@ -42,7 +54,22 @@ class Activity {
                 s.append(i + " ");
             }
         }
+        System.out.println(array.toString());
         System.out.println(s.toString());
     }
 
+    private static void Sort(int[] start, int[] finish) {
+        for (int j = finish.length - 1; j > 0; j--) {
+            for (int i = 0; i < j; i++) {
+                if (finish[i] > finish[i + 1]) {
+                    int temp = finish[i];
+                    finish[i] = finish[i + 1];
+                    finish[i + 1] = temp;
+                    temp = start[i];
+                    start[i] = start[i + 1];
+                    start[i + 1] = temp;
+                }
+            }
+        }
+    }
 }
--- src/app/Cycle.java
+++ src/app/Cycle.java
@@ -3,28 +3,54 @@
  * @Author: nuso
  * @LastEditors: nuso
  * @Date: 2019-11-17 11:42:50
- * @LastEditTime: 2019-11-17 12:19:49
+ * @LastEditTime: 2019-12-06 10:27:06
  */
 
 package app;
 
 public class Cycle {
     public static void main(String args[]) {
-        int n = 8;  // 运动员人数（2^k）
-        int[][] table = new int[n][n];
-        cycle(table, n);
+        int n = 9; // 运动员人数（2^k）
+        int[][] table = cycle(n);
         PrintTable(table);
     }
 
-    public static void cycle(int[][] table, int n) {
+    public static int[][] cycle(int n) {
+        int[][] table;
+        int delete = -1;
+        // N 为奇数时计算 n = n + 1
+        if (n % 2 != 0) {
+            table = new int[n + 1][n + 1];
+            delete = n + 1;
+            // _cycle(table, n + 1);
+        } else {
+            table = new int[n][n];
+            // _cycle(table, n);
+        }
+        __cycle(table, n);
+        if (delete != -1) {
+            for (int i = 0; i < table.length; i++) {
+                for (int j = 0; j < table[0].length; j++) {
+                    if (table[i][j] == delete) {
+                        table[i][j] = 0;
+                    }
+                }
+                table[table.length - 1][i] = 0;
+            }
+        }
+        return table;
+    }
+
+    public static void _cycle(int[][] table, int n) {
+        int n_2 = n / 2;
+        if (n_2 != 1 && n_2 % 2 != 0)
+            n_2++;
         if (n == 1) {
             table[0][0] = 1;
             return;
-        } else {
-            cycle(table, n / 2);
         }
 
-        int n_2 = n / 2;
+        _cycle(table, n_2);
 
         // 左上角复制到右下
         for (int i = n_2; i < n; i++) {
@@ -65,10 +91,71 @@ public class Cycle {
             }
             content.append("\n\n");
         }
-        
+
         System.out.println("== 循环赛日程表 =================>");
         System.out.println(head.toString());
         System.out.println(content.toString());
     }
 
+    private static void __cycle(int[][] a, int n) {
+        if (n == 1) {
+            a[0][0] = 1;
+            return;
+        }
+        if (odd(n)) {
+            __cycle(a, n + 1);
+            return;
+        }
+        __cycle(a, n / 2);
+        Copy(a, n);
+    }
+
+    private static boolean odd(int n) {
+        return n % 2 != 0;
+    }
+
+    private static void Copy(int[][] a, int n) // makecopy 与copy算法类似，但是要区分n/2为奇数或偶数的情形
+    {
+        if (n / 2 > 1 && odd(n / 2))
+            copyodd(a, n);
+        else
+            copy(a, n);
+    }
+
+    private static void copy(int[][] a, int n) {
+        int m = n / 2;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < m; j++) {
+                a[i][j + m] = a[i][j] + m;
+                a[i + m][j] = a[i][j + m];
+                a[i + m][j + m] = a[i][j];
+            }
+        }
+
+    }
+
+    private static void copyodd(int[][] a, int n) // 实现n/2为奇数时的复制
+    {
+        int[] b = new int[n];
+        int m = n / 2;
+        for (int i = 0; i < m; i++) {
+            b[i] = m + i;
+            b[m + i] = b[i];
+        }
+        for (int i = 0; i < m; i++) {
+            // 1-3行复制到4-6行（左半）
+            for (int j = 0; j <= m; j++) {
+                if (a[i][j] > m) {
+                    a[i][j] = b[i] + 1;
+                    a[m + i][j] = (b[i] + 1 + m) % n;
+                } else
+                    a[m + i][j] = a[i][j] + m;
+            }
+            // 构造最后两列
+            for (int j = 1; j < m; j++) {
+                a[i][m + j] = b[i + j] + 1;
+                a[b[i + j]][m + j] = i + 1;
+            }
+        }
+    }
 }
